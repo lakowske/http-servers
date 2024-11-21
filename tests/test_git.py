38493e5ce508329cfd797b68a66793c01b65ee2e
Seth Lakowske <lakowske@gmail.com>
2024-11-21 09:34:27 -0600

    Fix unit test.

--- tests/test_git.py
+++ tests/test_git.py
@@ -5,6 +5,7 @@ This module tests the http git server functionality.
 import unittest
 from http_servers.auth import UserCredential, from_passwd_file
 
+
 class TestGit(unittest.TestCase):
 
     def setUp(self):
@@ -17,6 +18,6 @@ class TestGit(unittest.TestCase):
         """Clones the test repository and checks if the cloned repository exists."""
         # Read the passwd file for a valid user
         users = from_passwd_file("/tmp/test_passwd")
-        
-        cmd = "git clone http://admin:
+
+        cmd = "git clone http://admin:"
         pass
