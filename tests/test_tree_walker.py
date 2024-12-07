80f72850b4c96610e54d997e3d60452aa30f9031
Seth Lakowske <lakowske@gmail.com>
2024-12-06 21:18:32 -0600

    Fix broken tests.  See if a PR system that blocks merges until a passing build occurs can be setup on github.

--- tests/test_tree_walker.py
+++ tests/test_tree_walker.py
@@ -2,7 +2,7 @@ from configuration.tree_walker import TreeWalker, TreeRenderer, TreeRemoval
 from configuration.tree_nodes import build_tree
 from configuration.app import Config, AdminContext
 
-tree_size = 27
+tree_size = 28
 
 
 def test_print_walker():
